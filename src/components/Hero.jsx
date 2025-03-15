422b98123a8189724959a9070bd92dbab6ad8678
Pallav0099 <pallavsworkspace@gmail.com>
2025-03-16 04:46:04 +0530

    hero section almost done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,16 +4,16 @@ import ParticleCanvas from "../design/ParticleCanvas";
 const Hero = () => {
     return (
         <div>
-            <div id="hero" className="bg-black-4 grid grid-cols-2 items-center px-10 relative z-10">
-                {/* Left Side */}
-                <div className="text-left">
-                    <p className="text-8xl font-bold text-white ml-30 mt-85">Hermeias</p>
-                    <p className=" text-2xl text-white py-2 ml-32">Open and Private.</p>
+            <div id="hero" className="bg-black-4 flex flex-col items-center text-center px-6 py-12 z- lg:grid lg:grid-cols-2 lg:items-center lg:text-left lg:px-10 lg:py-20">
+                {/* Right Side - Image*/}
+                <div className="flex justify-center mt-40 lg:justify-end lg:order-2">
+                    <img src={svg} alt="Hero" className="w-32 sm:w-40 md:w-48 lg:w-56" />
                 </div>
 
-                {/* Right Side */}
-                <div className="flex justify-end mr-30 mt-20">
-                    <img src={svg} alt="Hero" className="w-40" />
+                {/* Left Side - Text*/}
+                <div className="mt-10 lg:mt-85 lg:order-1">
+                    <p className="text-6xl sm:text-7xl md:text-8xl font-bold text-white lg:ml-30">Hermeias</p>
+                    <p className="text-lg sm:text-xl md:text-2xl text-white py-4 lg:ml-31">Open and Private.</p>
                 </div>
             </div>
             <ParticleCanvas />
