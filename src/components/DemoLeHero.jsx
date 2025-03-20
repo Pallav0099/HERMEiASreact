5769556192bd922fe4c9ecdd76b74583a3261337
Pallav0099 <pallavsworkspace@gmail.com>
2025-03-20 12:37:34 +0530

    demole preview

--- src/components/DemoLeHero.jsx
+++ src/components/DemoLeHero.jsx
@@ -18,7 +18,7 @@ const DemoLeHero = () => {
                     </div>
                 </div>
                 <div className="bg-black-4 flex items-center align-middle justify-center px-4 py-20 sm:px-6 sm:py-24 lg:px-8 lg:py-28 z-10">
-                    <img src={DemoLe} alt="DemoLe Ai" className="max-h-[60vh]" />
+                    <img src={DemoLe} alt="DemoLe Ai" className="max-h-[60vh] motion-blur-in-2xl motion-opacity-in-0 motion-duration-1500" />
                 </div>
             </div>
             <StarsBackground />
